98e5a939ff22c8442e3a006de337f691a29e94e6
Grady Rueffer <113617132+GradyRuefferOutlook@users.noreply.github.com>
2025-04-03 15:02:29 -0400

    clean up

--- game/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../connect.php";
-session_start();
-
-echo("<h1>Hi! If you've gone here expecting something, try this:</h1> <br><br>");
-echo("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank'>Try This</a> <br><br>");
-echo("<h1>Or go back to the menu:</h1> <br><br>");
-echo("<a href='../menu'>Menu</a>");
-?>
